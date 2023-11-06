2a108391147998c3adbbc73a9483312b1e945ee6
pampae <facundoetienot@gmail.com>
2023-11-06 14:49:35 -0300

    modo claro oscuro

--- forms/EditarDato.php
+++ forms/EditarDato.php
@@ -14,9 +14,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>editar paciente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script defer src="colorToggle.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
   <body>
     <h1 class= "bg-warning p-2 text-white text-center">editar paciente</h1>
+    <button class="light-mode-btn" aria-label="activar modo oscuro"><i class="fa-solid fa-moon"></i></button>
+    <button class="dark-mode-btn" aria-label="activar modo claro"><i class="fa-solid fa-sun"></i></button>
     <br>
  <div class="container">
  <form action="../CRUD/Editar.php" method="POST">
--- index.php
+++ index.php
@@ -6,29 +6,38 @@
     <title>NUTRICION</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
     crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script defer src="colorToggle.js"></script>
+    
   </head>
   <body>
     <h1 CLASS="bg-warning p-2 text-white text-center">NUTRICION</h1>
     <br>
     <div class="container">
     <a href="forms/agregar_paciente.php" class="btn btn-danger"> Agregar paciente</a>
-    
+    <button class="light-mode-btn" aria-label="activar modo oscuro"><i class="fa-solid fa-moon"></i></button>
+    <button class="dark-mode-btn" aria-label="activar modo claro"><i class="fa-solid fa-sun"></i></button>
     </div>
     <br>
-    <div class="container bg-light p-3 border border-dark rounded">
+    <div class="container bg-light p-3 border border-dark rounded ">
         <h1>Lista de pacientes</h1>
         <table class="table">
             <thead class="table-dark">
                 
     <tr>
-      <th scope="col">id_pacientes</th>
-      <th scope="col">nombre</th>
-      <th scope="col">apellido</th>
-      <th scope="col">dni</th>
-      <th scope="col">os</th>
-      <th scope="col">tel</th>
-      <th scope="col">localidad</th>
-      <th scope="col">talla</th>
+      <th scope="col">ID Paciente</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">DNI</th>
+      <th scope="col">O.S.</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Localidad</th>
+      <th scope="col">Talla</th>
+      <th scope="col"></th>
+      
     </tr>
   </thead>
   <tbody>
